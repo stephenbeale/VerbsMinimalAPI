b482d524e0577b9dc5e48f21255678ab01ec4350
Steve Beale <steve@imagitech.co.uk>
2025-03-05 07:30:30 +0000

    Authentication issues fixed. Actually calls API, but returns 404.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using VerbsMinimalAPI;
 using VerbsMinimalAPI.DataAccess;
 
@@ -5,24 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer()
+builder.Services
+    .AddEndpointsApiExplorer()
     .AddSwaggerGen()
-    .AddSingleton<IDataAccess, DataAccess>();
-
-//From AI - an attempt to allow connection to a non-docker project
-builder.Services.AddCors(options =>
-{
-	options.AddPolicy("AllowBlazorOrigin",
-		builder => builder.WithOrigins("https://localhost:YOUR_BLAZOR_PORT")
-						  .AllowAnyHeader()
-						  .AllowAnyMethod());
-});
-
+    .AddSingleton<IDataAccess, DataAccess>()
+    .AddAuthorization()
+    .AddAuthentication()
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);    
 
 var app = builder.Build();
 
 //From AI
-app.UseCors("AllowBlazorOrigin");
+//app.UseCors("AllowBlazorOrigin");
+
+//From error on Swagger
+app.UseAuthorization();
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
