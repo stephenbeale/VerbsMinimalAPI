be599d0aac78813f41b8bfa9d17f2b5de97b54db
Steve Beale <steve@imagitech.co.uk>
2025-03-07 16:54:48 +0000

    Connection string changes with Simon's help. He also suggested rolling back Sql client to the older one (now installed) to get around some weird connection issues earlier. Also removed authorise decorator since I don't have authorisation set up.
    Dapper needs aliasing if table names do not match property names in your local project, otherwise it can't handle the discrepancy, so us AS on the query.

--- DataAccess/DataAccess.cs
+++ DataAccess/DataAccess.cs
@@ -1,40 +1,42 @@
 ﻿using Dapper;
-using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 using VerbsMinimalAPI.Entities;
 
 namespace VerbsMinimalAPI.DataAccess
 {
-    public class DataAccess : IDataAccess
-    {
-        private readonly IConfiguration _configuration;
+	public class DataAccess : IDataAccess
+	{
+		private readonly IConfiguration _configuration;
 
-        public DataAccess(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+		public DataAccess(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
 
-        public async Task<Verb> GetVerb(int id)
-        {
-            using (IDbConnection dbConnection = new SqlConnection(_configuration.GetConnectionString("Default")))
-            {
-                string query = @"
-                    SELECT Verb
+		public async Task<Verb> GetVerbById(int id)
+		{
+			var connectionString = _configuration.GetConnectionString("Default");
+
+			using (IDbConnection dbConnection = new SqlConnection(connectionString))
+			{
+				string query = @"
+                    SELECT ID, Verb AS Name
                     FROM dbo.Verbs
                     WHERE ID = @id
                 ";
 
-                try
-                {
-                    var result = await dbConnection.QueryAsync<Verb>(query, new { id });
-                    return result.FirstOrDefault();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"{ex.Message}");
-                    throw;
-                }
-            }
-        }
+				try
+				{
+					var result = await dbConnection.QueryAsync<Verb>(query, new { id });
+					return result.FirstOrDefault();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"{ex.Message}");
+					throw;
+				}
+			}
+		}
 	}
 }
--- MapApiCalls.cs
+++ MapApiCalls.cs
@@ -8,18 +8,18 @@
         {
             public static IApplicationBuilder MapApiCalls(this WebApplication app)
             {
-                app.MapGet("/verb/{id}", GetVerb).WithName(nameof(GetVerb));
+                app.MapGet("/verb/{id}", GetVerbById).WithName(nameof(GetVerbById));
                 return app;
             }
 
-            [Authorize]
-            public static async Task<IResult> GetVerb(ILoggerFactory loggerFactory, IDataAccess dataAccess, [FromRoute] int id)
+            //[Authorize]
+            public static async Task<IResult> GetVerbById(ILoggerFactory loggerFactory, IDataAccess dataAccess, [FromRoute] int id)
             {
-                ILogger logger = loggerFactory.CreateLogger(nameof(GetVerb));
+                ILogger logger = loggerFactory.CreateLogger(nameof(GetVerbById));
 
                 try
                 {
-                    var verb = await dataAccess.GetVerb(id);
+                    var verb = await dataAccess.GetVerbById(id);
                     return Results.Ok(verb);
                 }
                 catch (Exception ex)
